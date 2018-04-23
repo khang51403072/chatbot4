3a1eacac9fe2b281fda2fadae351cf7d62eb0a4e
khang51403072 <khang51403072@gmail.com>
2018-04-24 03:24:05 +0700

    initing chatbot

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
-'use strict'
+import { METHODS } from 'http';
 
+'use strict'
+let token = "EAADNmxtKabABAPp5k21aV6VQZAQNK7DLO0HMC8wLxQrLZBXCUJZC8nlFFITVCxEf8uPNEY3FqfZBs2KgTq6HgpsFZC2rt4z6mI43AT2NPHIASQD9wjcAD8272NccKLBYxPhTK3PcXs0BU4nvQg19ZCdGAxd1jByKxoZAml4nyl07gZDZD"
 const express = require('express')
 const bodyParser = require('body-parser')
 const request = require('request')
@@ -24,6 +26,41 @@ app.get('/webhook', function(req,res){
     res.send("Wrong token")
 })
 
+
+app.post('/webhook/',function(req,res){
+    let messaging_events = req.body.entry[0].messaging
+    for(let i =0; i< messaging_events.length;i++){
+        let event = messaging_events[i]
+        let sender = event.sender.id
+        if(event.message && event.message.text){
+            let text  = event.message.text
+            sendText(sender, "Test echo: " + text.substring(0,100))
+
+        }
+    }
+    res.sendStatus(200)
+})
+function sendText(sender, text)
+{
+    let messageData = {text:text}
+    request({
+        url: "https://graph.facebook.com/v2.6/messages"
+        qs: {access_token: token}
+        METHODS: "POST"
+        json: {
+            recipient: {id,sender},
+            message: messageData
+      }, function(error, respone, body){
+          if(error){
+              console.log("sending error")
+          }else if (respone.body.error)
+          {
+              console.log("respone body error")
+          }
+
+      }
+      })
+}
 app.listen(app.get('port'),function(){
     console.log("running: port")
 })
